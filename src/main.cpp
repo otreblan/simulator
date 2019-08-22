09edc04d8733f025ac9b10d92ae22929153c441d
otreblan <otreblain@gmail.com>
2019-08-21 21:43:49 -0500

    Arg

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <getopt.h>
 #include "config.h"
+
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello, world " << PACKAGE_VERSION << "\n";
+	// Argument counter
+	int cc;
+
+	// No errors from getopt (flag)
+	opterr=0;
+
+	// Argument parsing loop
+	while(true)
+	{
+		// Declaration of long arguments
+		static struct option options[] =
+		{
+			{"help", no_argument, 0, 'h'},
+			{"output", required_argument, 0, 'o'},
+			{0,0,0,0}
+		};
+
+		int option_index = 0;
+
+		// The actual parsing
+		cc = getopt_long(argc, argv, "ho:", options, &option_index);
+
+		// No more parseable arguments
+		if(cc == -1)
+		{
+			break;
+		}
+		// Argument switch table
+		switch(cc)
+		{
+			case 'h':
+				std::cout << "Ayuda\n";
+				break;
+			case 'o':
+				std::cout << "-->" << optarg << '\n';
+				break;
+			case '?':
+				std::cerr << "No\n";
+				break;
+		}
+	}
 	return 0;
 }
